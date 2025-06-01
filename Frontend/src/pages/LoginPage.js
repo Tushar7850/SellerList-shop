98920d697089baa517655e4f46294668dcda0525
Tushar7850 <sawanttushar796@gmail.com>
2025-06-01 16:40:30 +0530

    changes

--- Frontend/src/pages/LoginPage.js
+++ Frontend/src/pages/LoginPage.js
@@ -19,7 +19,7 @@ const schema = yup.object().shape({
     .required("Email is required"),
   password: yup
     .string()
-    .min(6, "Password must be at least 6 characters")
+    .min(8, "Password must be at least 8 characters")
     .required("Password is required"),
 });
 
